3f085b5232f5b1fa5a41fd9186373af201433c9c
ElchinNasirov <nasirov.elchin@gmail.com>
2025-06-28 20:52:02 -0700

    working on react context to pass the product data to components

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import "./css/ShopCategory.css";
 
-const ShopCategory = () => {
-  return <div>ShopCategory</div>;
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+    </div>
+  );
 };
 
 export default ShopCategory;
